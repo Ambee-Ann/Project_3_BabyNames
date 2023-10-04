545a7df1c990b53be28289e147df1bb6b8423756
unknown <victorh360@gmail.com>
2023-10-03 20:20:25 -0400

    Added starter js files

---
+++ static/js/logic.js
@@ -0,0 +1,28 @@
+
+
+var map = L.map('map').setView([37.0902, -95.7129], 4);
+
+// Add a base layer (e.g., OpenStreetMap)
+
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(statesData, {style: style}).addTo(map);
+
+
